e82d3ad8c07e2e94b9418bd41f175e9b645531f8
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2022-11-02 06:11:29 +0100

    Top 5 and bottom 20 by amount spent

--- earliest_orders.sql
+++ earliest_orders.sql
@@ -1,9 +1,28 @@
 
 -- SQL ORDER BY --
 
+
+--EARLIEST ORDERS--
 /*The 10 earliest orders taken from the Parch & Posey database*/
 
 SELECT id, occurred_at, total_amt_usd
 FROM orders
 ORDER BY occurred_at
 LIMIT 10;
+
+--TOP 5 BY AMOUNT--
+/*Top 5 orders based on the amount spent in USD taken from the Parch & Posey database*/
+
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd
+LIMIT 5;
+
+
+--BOTTOM 20 BY AMOUNT--
+/*Bottom 20 orders based on the amount spent in USD taken from the Parch & Posey database*/
+
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd
+LIMIT 20;
